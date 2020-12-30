64c241152aa1db7f8a3891b31fc334df4b37a581
Wally Jones <wally@wallyjones.com>
2020-12-29 23:23:31 -0500

    Set git remote to ssh after pulling

--- up.sh
+++ up.sh
@@ -5,11 +5,14 @@ rm .zsh_history
 if [ -d ~/.dotfiles ]; then
         cd ~/.dotfiles
         git pull --no-ff
-        cd ~
 else
         git clone https://github.com/imwally/dotfiles.git .dotfiles
+        cd ~/.dotfiles;
+        git remote rm origin
+        git remote add origin git@github.com:imwally/dotfiles.git
 fi
 
+cd ~
 for f in ~/.dotfiles/.??*; do
         ln -sf $f .
 done
