1f9e7afcc12b1901a0539ea97ac027137e48ef7d
Wally Jones <wally@wallyjones.com>
2020-12-30 22:09:06 -0500

    Fixes via shellcheck

--- up.sh
+++ up.sh
@@ -3,16 +3,16 @@
 rm .zsh_history
 
 if [ -d ~/.dotfiles ]; then
-        cd ~/.dotfiles
+        cd ~/.dotfiles || exit
         git pull --no-ff
 else
         git clone https://github.com/imwally/dotfiles.git .dotfiles
-        cd ~/.dotfiles;
+        cd ~/.dotfiles || exit
         git remote rm origin
         git remote add origin git@github.com:imwally/dotfiles.git
 fi
 
-cd ~
+cd ~ || exit
 for f in ~/.dotfiles/.??*; do
-        ln -sf $f .
+        ln -sf "$f" .
 done
