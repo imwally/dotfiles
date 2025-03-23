931265865d8c1d1d0606398e59947d4ad9e7a664
Wally Jones <wally@wallyjones.com>
2025-03-23 17:21:45 -0400

    Use var for directory

--- up.sh
+++ up.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+DOTFILES_DIR=~/.dotfiles
+
 rm .zsh_history
 
-if [ -d ~/.dotfiles ]; then
-        cd ~/.dotfiles || exit
+if [ -d $DOTFILES_DIR ]; then
+        cd $DOTFILES_DIR || exit
         git pull --no-ff
 else
-        git clone https://github.com/imwally/dotfiles.git .dotfiles
-        cd ~/.dotfiles || exit
+        git clone https://github.com/imwally/dotfiles.git $DOTFILES_DIR
+        cd $DOTFILES_DIR || exit
         git remote rm origin
         git remote add origin git@github.com:imwally/dotfiles.git
 fi
 
 cd ~ || exit
-for f in ~/.dotfiles/.??*; do
+for f in $DOTFILES_DIR/.??*; do
         ln -sf "$f" .
 done
